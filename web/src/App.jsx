7163e83324b22abdea97c5dc96ba3ab21664c753
SUJOY GARAI <sujoycode999@gmail.com>
2025-10-27 02:28:58 +0530

    ...

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,3 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navber from './components/Navber'
 import Login from './components/Login'
--- web/src/main.jsx
+++ web/src/main.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App';
 import Text from './__text__/text';
 import Layout01 from './layout/Layout01';
 import Home from './pages/Home';
